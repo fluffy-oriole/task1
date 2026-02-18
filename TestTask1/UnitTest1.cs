f2d34e3baab0c342c59f0735287d532d7f5bcf45
Flffy-Oriole <aarrtteemm2006@gmail.com>
2026-02-19 00:48:46 +0800

    сделаны тесты

--- TestTask1/UnitTest1.cs
+++ TestTask1/UnitTest1.cs
@@ -1,4 +1,13 @@
-﻿namespace TestTask1
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using task1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask1
 {
     public class Tests
     {
@@ -8,9 +17,39 @@
         }
 
         [Test]
-        public void Test1()
+        public void RubAndCents()
         {
-            Assert.Pass();
+            var expected = new int[] { 48, 75 };
+            var real = Logic.GetRubCents(4875);
+            CollectionAssert.AreEqual(expected, real);
+        }
+
+        [Test]
+        public void FirstWord()
+        {
+            var word = Logic.GetCentsWord(0);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(" ровно", word);
+        }
+
+        [Test]
+        public void SecondWord()
+        {
+            var word = Logic.GetCentsWord(13);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("13 копеек", word);
+        }
+
+        [Test]
+        public void ThirdWord()
+        {
+            var word = Logic.GetCentsWord(1);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("1 копейка", word);
+        }
+
+        [Test]
+        public void ForthWord()
+        {
+            var word = Logic.GetCentsWord(4);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("4 копейки", word);
         }
     }
 }
--- task1/Program.cs
+++ task1/Program.cs
@@ -1,36 +1,45 @@
 ﻿namespace task1
 {
+    public class Logic
+    {
+        public static int[] GetRubCents(int num) {
+            int rub = num / 100;
+            int cents = num % 100;
+            return [rub, cents];
+        }
+        public static String GetCentsWord(int cents)
+        {
+            if (cents == 0)
+            {
+                return " ровно";
+            }
+            else if (cents > 10 && cents < 20)
+            {
+                return (cents.ToString() + " копеек");
+            }
+            else if (cents % 10 == 1)
+            {
+                return (cents.ToString() + " копейка");
+            }
+            else if (cents % 10 > 1 && cents % 10 < 5)
+            {
+                return (cents.ToString() + " копейки");
+            }
+            else
+            {
+                return (cents.ToString() + " копеек");
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.Write("Введите число для преобразования: ");
             int num = int.Parse(Console.ReadLine());
-            int rub = num / 100;
-            int cents = num % 100;
-            Console.Write(rub.ToString() + " руб. ");
-            if (cents == 0)
-            {
-                Console.Write("ровно");
-            }
-            else if (cents > 10 && cents < 20)
-            {
-
-                Console.Write(cents.ToString() + " копеек");
-            }
-            else if (cents % 10 == 1)
-            {
-                Console.Write(cents.ToString() + " копейка");
-            }
-            else if (cents % 10 > 1 && cents % 10 < 5)
-            {
-                Console.Write(cents.ToString() + " копейки");
-            }
-            else
-            {
-                Console.Write(cents.ToString() + " копеек");
-            }
-
+            int rub = Logic.GetRubCents(num)[0];
+            int cents = Logic.GetRubCents(num)[1];
+            Console.Write(rub.ToString() + " руб. " + Logic.GetCentsWord(cents));
         }
     }
 }
