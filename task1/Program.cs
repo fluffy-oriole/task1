d8e3fd57c31ba3fb1ab2101780d9d8be5f9c6437
Flffy-Oriole <aarrtteemm2006@gmail.com>
2026-02-18 23:39:25 +0800

    добавлено склонение слова копейки

--- task1/Program.cs
+++ task1/Program.cs
@@ -8,7 +8,29 @@
             int num = int.Parse(Console.ReadLine());
             int rub = num / 100;
             int cents = num % 100;
-            Console.Write(rub.ToString() + " руб. " + cents + " копеек");
+            Console.Write(rub.ToString() + " руб. ");
+            if (cents == 0)
+            {
+                Console.Write("ровно");
+            }
+            else if (cents > 10 && cents < 20)
+            {
+
+                Console.Write(cents.ToString() + " копеек");
+            }
+            else if (cents % 10 == 1)
+            {
+                Console.Write(cents.ToString() + " копейка");
+            }
+            else if (cents % 10 > 1 && cents % 10 < 5)
+            {
+                Console.Write(cents.ToString() + " копейки");
+            }
+            else
+            {
+                Console.Write(cents.ToString() + " копеек");
+            }
+
         }
     }
 }
